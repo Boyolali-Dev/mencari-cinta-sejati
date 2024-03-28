489341c610c0e346f9bb96fdec70aed489a3a932
kasurbantal <kasurbantal880@gmail.com>
2024-03-28 09:37:29 +0700

    Mengatur layout card

--- todo-list-nextjs/src/app/todocard.jsx
+++ todo-list-nextjs/src/app/todocard.jsx
@@ -1,18 +1,25 @@
 import styles from './todocard.module.css'
 
 const TodoCard = () => {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    const formattedDate = `${dd}/${mm}/${yyyy}`
+
     return (
         <div className={styles.taskCard}>
             <div className={styles.taskCardButtonContainer}>
                <button className={styles.cancelButton}>X</button>
             </div>
             <div className={styles.taskCardContent}>
-                <p>Kegiatan: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Deadline: tanggal ditentukan</p>
+                <h3>Activity:</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div className={styles.taskCardButtonContainer}>
-                <a href="#" className={styles.taskCardButton}>Completed</a>
-                <a href="#" className={`${styles.taskCardButton} ${styles.inProgressButton}`}>In Progress</a>
+                <p>Deadline: {formattedDate}</p>
+                <button className={styles.taskCardButton}>Completed</button>
+                <button className={`${styles.taskCardButton} ${styles.inProgressButton}`}>In Progress</button >
             </div>
         </div>
     )
