4896aa282cdc0c56f3efd7feea95c5cb2e945a6e
Indra Septama <36232396+indraseptama@users.noreply.github.com>
2024-04-23 21:20:09 +0700

    feat: add todo card (#6)
    * Install npm
    
    * Membuat todocard.jsx
    
    * Membuat button
    
    * Menambah button cancel
    
    * Mengatur layout card
    
    * Mengatur layout card
    
    * Menambah componen to do card
    
    * Mengatur layout card pada halaman page
    
    * Menambahkan fungsi pada setiap tombol
    
    * Menambahkan fungsi delete card
    
    * Merapikan coding
    
    * Memperbaiki pemanggilan className
    
    * Mengganti page
    
    * Mengganti page.module.css
    
    ---------
    
    Co-authored-by: kasurbantal <kasurbantal880@gmail.com>

---
+++ todo-list-nextjs/src/components/todocard/todocard.jsx
@@ -0,0 +1,40 @@
+import classNames from 'classnames'
+import styles from './todocard.module.css'
+import { TodoCardType } from './enum';
+
+const TodoCard = ({ id, activity, deadline, type, onComplete, onInProgress, onHold, onDelete }) => { // Tambahkan prop onComplete
+    return (
+      <div className={styles.taskCard}>
+        <div className={styles.taskCardButtonContainer}>
+            {(type === TodoCardType.Todo || type === TodoCardType.Completed) && (
+            <button className={styles.cancelButton} onClick={() => onDelete(id)}>X</button>
+            )}
+        </div>
+        <div className={styles.taskCardContent}>
+          <h3>Activity:</h3>
+          <p>{activity}</p>
+        </div>
+        <div className={styles.taskCardButtonContainer}>
+          <p>Deadline: {deadline}</p>
+          {/* Tampilkan tombol Completed hanya jika kartu bukan tipe Completed */}
+          {type !== TodoCardType.Completed && (
+            <button className={styles.taskCardButton} onClick={() => onComplete(id)}>Completed</button>
+          )}
+          {/* Tampilkan tombol sesuai dengan tipe kartu */}
+          {(type === TodoCardType.Todo || type === TodoCardType.Hold) && (
+            <button className={classNames(
+              styles.taskCardButton,
+              styles.inProgressButton)} onClick={() => onInProgress(id)}>In Progress</button>
+          )}
+          {type === TodoCardType.InProgress && (
+            <button className={classNames(
+              styles.taskCardButton,
+              styles.holdButton
+            )} onClick={() => onHold(id)}>Hold</button>
+          )}
+        </div>
+      </div>
+    );
+  };
+  
+  export default TodoCard;
