a2b3f52dce423341435aec8a8915b6146e467d22
kasurbantal <kasurbantal880@gmail.com>
2024-05-13 20:35:20 +0800

    Refactor addTodo.jsx ke addTodo.tsx, todocard.jsx dan enum js ke todocard.tsx dan enum.ts

--- todo-list-nextjs/src/components/todocard/enum.ts
+++ todo-list-nextjs/src/components/todocard/enum.ts
@@ -1,13 +1,8 @@
-	interface TodoCardTypeProps {
-		todo: string;
-		inprogress: string;
-		completed: string;
-		hold: string;
-	}
-	
-	export const TodoCardType = {
-		Todo      : "todo",
-		InProgress: "inprogress",
-		Completed : "completed",
-		Hold      : "hold"
-	}
+enum TodoCardType {
+	Todo = "todo",
+	InProgress = "inprogress",
+	Completed = "completed",
+	Hold = "hold"
+  }
+  
+  export default TodoCardType;  
